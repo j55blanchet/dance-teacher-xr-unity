c99fc6ebfffe12d3208f06531a36c907cdaa6d59
Julien Blanchet <j55blanchet@gmail.com>
2022-09-30 13:02:27 -0400

    Fixing minor BVH problems

--- motion-pipeline/motion_extraction/stepone_get_holistic_data.py
+++ motion-pipeline/motion_extraction/stepone_get_holistic_data.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 import sys
-from typing import Callable, Generator, TypeVar
+from typing import Callable, Generator, TypeVar, Optional
 import cv2
+from matplotlib import pyplot as plt
 import mediapipe as mp
 import numpy as np
 import pandas as pd
@@ -9,7 +10,6 @@ from functools import reduce
 import csv
 import logging
 
-from pyparsing import Optional
 from .utils import throttle
 
 PoseLandmark: mp.solutions.mediapipe.python.solutions.holistic.PoseLandmark = mp.solutions.holistic.PoseLandmark
@@ -78,19 +78,51 @@ def process_video(
         merged_data_csv = csv.writer(merged_data_file)
         for frame_i, (_, frame_count, frame_data, image) in enumerate(_perform_by_frame(video_path, holistic_solution.process)):
 
-            image.flags.writeable = True
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-            mp_drawing.draw_landmarks(
-                image,
-                frame_data.pose_landmarks,
-                mp.solutions.holistic.POSE_CONNECTIONS,
-                landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style(),
-            )
+
             # cv2.imshow(f'Frame {frame_i}', image)
             # cv2.waitKey(500)
+
+            
+            
             if frame_output_folder is not None:
+                image.flags.writeable = True
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                mp_drawing.draw_landmarks(
+                    image,
+                    frame_data.pose_landmarks,
+                    mp.solutions.holistic.POSE_CONNECTIONS,
+                    landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style(),
+                )
                 frame_output_folder.mkdir(parents=True, exist_ok=True)
-                cv2.imwrite(f'{frame_output_folder}/{video_path.stem}/{video_path.stem}_{frame_i:0{len(str(int(frame_count)))}}.jpg', image)
+                out_path_2d = f'{frame_output_folder}/{video_path.stem}_2d/{video_path.stem}_{frame_i:0{len(str(int(frame_count)))}}.jpg'
+                Path(out_path_2d).parent.mkdir(parents=True, exist_ok=True)
+                cv2.imwrite(out_path_2d, image)
+
+                fig = plt.figure(f'{holistic_data_filepath.name}-frame{frame_i}')
+                ax = fig.add_subplot(projection='3d')
+
+                world_landmarks  = [
+                    tuple([PoseLandmark(landmark_i), (frame_data.pose_world_landmarks.landmark[landmark_i] if frame_data.pose_world_landmarks else None)])
+                    for landmark_i
+                    in range(len(PoseLandmark))
+                ]
+                x, y, z = list(zip(*[
+                    (landmark.x, -landmark.y, -landmark.z)
+                    for _, landmark
+                    in world_landmarks
+                ]))
+
+                # Swap x and y so that default camera angle is good
+                ax.set_xlabel('X')
+                ax.set_ylabel('Y')
+                ax.set_zlabel('Z')
+                ax.scatter(x, y, z)
+                out_path = f'{frame_output_folder}/{video_path.stem}_3d/{video_path.stem}_{frame_i:0{len(str(int(frame_count)))}}.png'
+                Path(out_path).parent.mkdir(parents=True, exist_ok=True)
+                # plt.show(block=True)
+                plt.savefig(out_path)
+                plt.close()
+
 
             print_progress(frame_i, frame_count)
             if frame_i == 0:
@@ -117,15 +149,14 @@ def process_video(
                     [
                         # We want to remap x, y, z. 
                         #   > The default has negative y being up, positive x being right, and pozitive z being away from the camera.
-                        #   > OLD We want z being up, x being forward, and y being right.
-                        #   > We actually want y being up, x being left, and z being forward.
-                        #   So x <- -x
+                        #   > We actually want y being up, x being left, and z being forward (towards camera).
+                        #   So x <- x
                         #      y <- -y
                         #      z <- -z
                         [ 
                             lm.x,
                             -lm.y, 
-                            lm.z,
+                            -lm.z,
                             lm.visibility
                         ] if lm is not None else [None, None, None, None]
                         for lm in 
@@ -186,7 +217,13 @@ def main():
         args.output_folder.mkdir(parents=True)
 
     video_folder = Path(args.video_folder)
-    for video_path in video_folder.glob('*.mp4'):
+    video_paths = video_folder
+    if video_folder.is_dir():
+        video_paths = video_folder.glob('*.mp4')
+    else:
+        video_paths = video_folder.parent.glob(video_folder.name)
+
+    for video_path in video_paths:
         process_video(
             video_path, 
             holistic_solution=holistic_solution, 
