cb5c4b3a5c2c4030021f7a2f4e1f7cd2b2591e18
Julien Blanchet <j55blanchet@gmail.com>
2022-11-29 16:57:05 -0500

    Switched to async requests

--- motion-pipeline/motion_extraction/teleoperation/NaoTeleoperationStreamer.py
+++ motion-pipeline/motion_extraction/teleoperation/NaoTeleoperationStreamer.py
@@ -8,6 +8,8 @@ import json
 import urllib3
 import urllib3.exceptions
 
+from concurrent.futures import ThreadPoolExecutor, wait
+
 @dataclass
 class NaoTeleoperationListener:
     name: str
@@ -21,7 +23,13 @@ class NaoTeleoperationStreamer:
         self.traj_output_provider = NaoTrajectoryOutputProvider('temp/nao_ctl.csv')
         self.frame_i = 0
         self.listeners: List[NaoTeleoperationListener] = []
+        self.thread_pool = ThreadPoolExecutor(max_workers=10)
+        self.tasks = []
         self.http = urllib3.PoolManager()
+
+    def __del__(self):
+        wait(self.tasks)
+        self.thread_pool.shutdown()
 
     def register_listener(self, name: str, listener_ip: Union[int, Literal['localhost']], listener_port: int):
         self.listeners.append(NaoTeleoperationListener(name, listener_ip, listener_port))
@@ -37,19 +45,21 @@ class NaoTeleoperationStreamer:
         self.forward_to_listeners(nao_ctl)
 
     def forward_to_listeners(self, nao_ctl: pd.Series):
-        
         ctl = nao_ctl.to_dict()
         ctl_str = json.dumps(ctl).encode('utf-8')
+        for i in range(len(self.listeners)):
+            self.tasks.append(self.thread_pool.submit(self.forward_to_listener, ctl_str, i))
 
-        for listener in self.listeners:
-            try:
-                r = self.http.request(
-                    'POST', 
-                    f'{listener.protocol}://{listener.ip}:{listener.port}/nao_ctl',
-                    body=ctl_str,
-                    headers={'Content-Type': 'application/json'}
-                )
-            except urllib3.exceptions.ProtocolError as e:
-                print(f'Error forwarding to listener {listener.name}: {e}')
+    def forward_to_listener(self, nao_ctl_str: pd.Series, i: int):
+        listener = self.listeners[i]
+        try:
+            r = self.http.request(
+                'POST', 
+                f'{listener.protocol}://{listener.ip}:{listener.port}/nao_ctl',
+                body=nao_ctl_str,
+                headers={'Content-Type': 'application/json'}
+            )
+        except urllib3.exceptions.ProtocolError as e:
+            print(f'Error forwarding to listener {listener.name}: {e}')
 
-            # json.loads(r.data.decode('utf-8'))['json']
+        # json.loads(r.data.decode('utf-8'))['json']
