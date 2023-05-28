0b370392ef424de343ca84d1645e403b03b1df95
Julien Blanchet <j55blanchet@gmail.com>
2023-05-28 08:46:25 -0400

    Starting to integrate compexity into dance trees

---
+++ motion-pipeline/motion_extraction/complexity_analysis/complexity_tools.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+import pandas as pd
+from ..dancetree.DanceTree import DanceTree
+
+
+
+def find_complexity_df(clip_relative_path: Path):
+    return pd.DataFrame()
+
+def add_complexity_to_dancetree(
+        tree: DanceTree,    
+    ):
+    
+    return tree
