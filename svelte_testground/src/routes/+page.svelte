31c61243256bff9448128e3401256e663b41be43
Julien Blanchet <j55blanchet@gmail.com>
2023-06-20 00:47:38 -0400

    adding data bundler, tree visual

--- svelte_testground/src/routes/+page.svelte
+++ svelte_testground/src/routes/+page.svelte
@@ -1,9 +1,21 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+<script lang="ts">
+	import { dances, danceTrees, makeDanceTreeSlug } from '$lib/dances-store';
+	import type { DanceTree, Dance } from '$lib/dances-store';
 
-	import { dances } from '$lib/dances-store';
+	let selectedDance: Dance | null = null;
+
+	let availableDanceTrees: Array<DanceTree> = [];
+	
+	// @ts-ignore
+	$: availableDanceTrees = danceTrees[selectedDance?.clip_relativepath] ?? [];
+
+	function toggleSelectDance(dance: Dance) {
+		if (selectedDance === dance) {
+			selectedDance = null;
+		} else {
+			selectedDance = dance;
+		}
+	}
 </script>
 
 <svelte:head>
@@ -16,23 +28,42 @@
 		LearnThatDance 2.0
 	</h1>
 
-	<p>Pick a dance</p>
-	<ul>
-		{#each $dances as dance (dance.clip_relativepath)}
-		<li class="outlined thin">{dance.title}</li>
-		{/each}
-	</ul>
+	
+	<div class="cols">
+		<div class="col dance-picking ta-center" style="max-width: 40ch;">
+			<p>Pick a dance</p>
+			<ul>
+				{#each dances as dance (dance.clip_relativepath)}
+				<li class="outlined thin" 
+					class:selected={dance === selectedDance}
+					on:click={(event) => toggleSelectDance(dance)}>{dance.title}</li>
+				{/each}
+			</ul>
+		</div>
+		{#if selectedDance}
+		<div class="col" style="max-width: 40ch;">
+			<p>{selectedDance.title}</p>
+			<ul>
+				{#each availableDanceTrees as danceTree (danceTree.clip_relativepath)}
+				<a href={"/teachlesson/" + makeDanceTreeSlug(danceTree)}><li class="outlined thin">{danceTree.tree_name}</li></a>
+				{/each}
+			</ul>
+		</div>
+		{/if}
+	</div>
+	
 	<!-- <Counter /> -->
 </section>
 
 <style>
 	section {
+		width: 100%;
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
 		flex: 0.6;
-		margin: 0 auto;
+		margin: 0 1rem;
 	}
 	p {
 		font-size: 1.1rem;
@@ -41,8 +72,15 @@
 		
 	}
 
+	ul li.selected {
+		background-color: #dedede;
+		box-shadow: 0 0 2px #000;
+		scale: 1.04 1;
+	}
+
 	ul {
 		margin-top: 0;
+		padding: 0;
 	}
 
 	ul li {
@@ -56,7 +94,7 @@
 
 	ul li:not(:last-child) {
 		margin-bottom: 0.15em;
-	}
+}
 
 	ul li:hover {
 		cursor: pointer;
