2027758d1d59f0d18433b010b8c76e741be26cb3
Julien Blanchet <j55blanchet@gmail.com>
2023-06-22 09:33:26 -0700

    Updating fallback for gh-pages

--- svelte_testground/vite.config.ts
+++ svelte_testground/vite.config.ts
@@ -2,7 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	base: '/dance-teacher-xr-unity/',
 	plugins: [sveltekit()],
 	worker: {
 		// format: 'es',
