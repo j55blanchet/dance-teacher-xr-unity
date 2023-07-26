69c9c7745225f6907a27ec5b3dd3a9089a7b32d2
Julien Blanchet <j55blanchet@gmail.com>
2023-07-26 12:48:47 -0400

    Implementing practice page layout

--- svelte_testground/src/lib/PracticePage.svelte
+++ svelte_testground/src/lib/PracticePage.svelte
@@ -1,22 +1,56 @@
 <script lang="ts">
 import VirtualMirror from "./VirtualMirror.svelte";
 
+import type { Dance } from "./dances-store";
+import { getDanceVideoSrc } from "./dances-store";
+
+export let dance: Dance;
+let videoElement: HTMLVideoElement;
+let videoCurrentTime: number = 0;
+let videoPlaybackSpeed: number = 1;
+let danceSrc: string = '';
+
+$: {
+    danceSrc = getDanceVideoSrc(dance);
+}
+
 </script>
 
 
 <section>
-    <VirtualMirror />    
+    <div>
+        <video bind:this={videoElement}
+               bind:currentTime={videoCurrentTime}
+               bind:playbackRate={videoPlaybackSpeed}>
+            <source src={danceSrc} type="video/mp4" />
+        </video>
+    </div>
+    <div>
+        <VirtualMirror />  
+    </div>
 </section>
 
 <style lang="scss">
 
 section {
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
     align-items: center;
-    justify-content: center;
+    justify-content: stretch;
     height: 100%;
     width: 100%;
+
+    & > div {
+        position: relative;
+        flex-grow: 1;
+        flex-shrink: 1;
+        flex-basis: 1rem;
+        max-width: 100%;
+        max-height: 100%;
+        border-radius: 0.5em;
+        display: flex;
+
+    }
 }
 
 </style>
