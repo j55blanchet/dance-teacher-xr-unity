c7007b13af9955dd06599ff5153325a481245373
Julien Blanchet <j55blanchet@gmail.com>
2023-08-03 14:57:22 -0400

    Implementing comparison algorithm

--- svelte_testground/src/lib/ai/Evaluation.ts
+++ svelte_testground/src/lib/ai/Evaluation.ts
@@ -1,10 +1,43 @@
+import { type Pose2DPixelLandmarks, PoseLandmarkIds } from "$lib/mediapipe-utils";
 
-/**
- * Compute the similarities between two 
- * @param refPose Reference Pose (row in CSV file)
- * @param mpResult User Pose (from mediapipe)
- * @returns Score indicating the similarity, between 0 and 1
- */
-export function compareSkeletons2DVector(refPose: any, mpResult: any) {
-    return 0;
+const ComparisonVectors = Object.freeze([
+    [PoseLandmarkIds.leftShoulder,  PoseLandmarkIds.rightShoulder],
+    [PoseLandmarkIds.leftShoulder,  PoseLandmarkIds.leftHip],
+    [PoseLandmarkIds.leftHip,       PoseLandmarkIds.rightHip],
+    [PoseLandmarkIds.rightHip,      PoseLandmarkIds.rightShoulder],
+    [PoseLandmarkIds.leftShoulder,  PoseLandmarkIds.leftElbow],
+    [PoseLandmarkIds.leftElbow,     PoseLandmarkIds.leftWrist],
+    [PoseLandmarkIds.rightShoulder, PoseLandmarkIds.rightElbow],
+    [PoseLandmarkIds.rightElbow,    PoseLandmarkIds.rightWrist]
+])
+
+function GetNormalizedVector(
+    pixelLandmarks: Pose2DPixelLandmarks,
+    srcLandmark: number,
+    destLandmark: number, 
+){
+    // TODO: utilize a vector arithmetic library?
+    const {x: sx, y: sy } = pixelLandmarks[srcLandmark]
+    const mag = Math.pow(Math.pow(sx, 2) + Math.pow(sy, 2), 0.5)
+    return [sx / mag, sy / mag]
+}
+
+export function compareSkeletons2DVector(
+    refLandmarks: Pose2DPixelLandmarks, 
+    userLandmarks: Pose2DPixelLandmarks
+): number {
+
+    let score = 0
+
+    // Compare 8 Vectors
+    for(const vecLandmarkIds of ComparisonVectors) {
+        const [srcLandmark, destLandmark] = vecLandmarkIds
+        const [refX, refY] = GetNormalizedVector(refLandmarks, srcLandmark, destLandmark)
+        const [usrX, usrY] = GetNormalizedVector(userLandmarks, srcLandmark, destLandmark)
+        const [dx, dy] = [refX - usrX, refY - usrY]
+        score += dx
+        score += dy
+    }
+
+    return score;
 }
