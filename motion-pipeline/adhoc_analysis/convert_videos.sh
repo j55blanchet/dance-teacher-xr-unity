f37b32c8e2e103c4ea07b6074465f51725535a85
Julien Blanchet <j55blanchet@gmail.com>
2023-08-07 13:28:30 -0400

    Finishing convert videos script

--- motion-pipeline/adhoc_analysis/convert_videos.sh
+++ motion-pipeline/adhoc_analysis/convert_videos.sh
@@ -1,11 +1,43 @@
+# convert_videos.sh
+#
+# This script will convert all videos from the input directory into
+# a mp4 with the h.264 codec and aac audio codec using ffmpeg. The 
+# output files will be placed in the output directory.
 
+# Input directory
+INPUT_DIR=$1
 
-VIDEO_FILE_LIST = $(shell ls *.mp4)
+# Output directory
+OUTPUT_DIR=$2
 
-all: $(VIDEO_FILE_LIST)
+# Check if input directory exists
+if [ ! -d "$INPUT_DIR" ]; then
+    echo "Input directory does not exist"
+    exit 1
+fi
 
-OUTPUT_FOLDER = ./output
+# Check if output directory exists
+if [ ! -d "$OUTPUT_DIR" ]; then
+    
+    # Create output directory
+    mkdir $OUTPUT_DIR
+fi
 
-$(VIDEO_FILE_LIST):
-    @echo "Converting $@ to h.264"
-    ffmpeg -i $@ -c:v libx264 $(OUTPUT_FOLDER)/$@.mp4
+# Loop through all files in input directory
+for file in $INPUT_DIR/*; do
+
+    # Get file name
+    filename=$(basename -- "$file")
+
+    # Get file extension
+    extension="${filename##*.}"
+
+    # Get file name without extension
+    filename="${filename%.*}"
+
+    # Check if file is a video
+    if [ "$extension" == "mp4" ]; then
+        # Convert video
+        ffmpeg -i $file -c:v libx264 -c:a aac $OUTPUT_DIR/$filename.mp4
+    fi
+done
