2bcc65ec449bfa0423c11cfc9b34097d6511696e
Sixuan Han <sixuan.han.25@dartmouth.edu>
2023-09-06 16:00:42 -0400

    used localstorage for persistance across page reloads

--- svelte-web-frontend/src/lib/model/settings.ts
+++ svelte-web-frontend/src/lib/model/settings.ts
@@ -1,14 +1,26 @@
 import { dev } from '$app/environment';
 import { writable } from 'svelte/store';
 
-export const debugMode = writable(dev);
+export const debugMode = writable(localStorage.debugMode === 'true');
 
-export const pauseInPracticePage = writable(false);
+export const pauseInPracticePage = writable(localStorage.pauseInPracticePage === 'true');
 
-export const debugPauseDurationSecs = writable(1.0);
+export const debugPauseDurationSecs = writable( parseFloat(localStorage.debugPauseDurationSecs) || 1.0);
 
-export const evaluation_GoodBadTrialThreshold = writable(4.0);
+export const evaluation_GoodBadTrialThreshold = writable(parseFloat(localStorage.evaluation_GoodBadTrialThreshold) || 4.0);
 
-export const feedback_YellowThreshold = writable(3.0);
+export const feedback_YellowThreshold = writable(parseFloat(localStorage.feedback_YellowThreshold) || 3.0);
 
-export const feedback_GreenThreshold = writable(4.2);
+export const feedback_GreenThreshold = writable(parseFloat(localStorage.feedback_GreenThreshold) || 4.2);
+
+debugMode.subscribe((value) => localStorage.debugMode = String(value));
+
+pauseInPracticePage.subscribe((value) => localStorage.pauseInPracticePage = String(value));
+
+debugPauseDurationSecs.subscribe((value) => localStorage.debugPauseDurationSecs = String(value));
+
+evaluation_GoodBadTrialThreshold.subscribe((value) => localStorage.evaluation_GoodBadTrialThreshold = String(value));
+
+feedback_YellowThreshold.subscribe((value) => localStorage.feedback_YellowThreshold = String(value));
+
+feedback_GreenThreshold.subscribe((value) => localStorage.feedback_GreenThreshold = String(value));
