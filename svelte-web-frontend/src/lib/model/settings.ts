ba2225ca80a3f679723970d5783da0008abe1a2c
Sixuan Han <sixuan.han.25@dartmouth.edu>
2023-09-07 14:38:01 -0400

    clean up code for settings

--- svelte-web-frontend/src/lib/model/settings.ts
+++ svelte-web-frontend/src/lib/model/settings.ts
@@ -1,26 +1,47 @@
 import { dev } from '$app/environment';
 import { writable } from 'svelte/store';
 
-export const debugMode = writable(localStorage.debugMode === 'true');
+const DEFAULT_SETTINGS = Object.freeze({
+   debugMode: false,
+   pauseInPracticePage : false,
+   debugPauseDurationSecs : 1.0,
+   evaluation_GoodBadTrialThreshold : 4.0,
+   feedback_YellowThreshold : 3.0,
+   feedback_GreenThreshold : 4.2
+})
 
-export const pauseInPracticePage = writable(localStorage.pauseInPracticePage === 'true');
+export const pauseDurationMin = 0.1;
+export const pauseDurationMax = 120;
+export const stepMin = 0.1;
+export const stepMax = 5;
 
-export const debugPauseDurationSecs = writable( parseFloat(localStorage.debugPauseDurationSecs) || 1.0);
+function createSettingsStore<T>(key: string, defaultValue: T, stringParser: (input: string) => T) {
+    const initialValue = localStorage[key] === undefined ? defaultValue : stringParser(localStorage[key]);
+    const settingsStore = writable(initialValue);
+    settingsStore.subscribe((value) => localStorage[key] = String(value));
+    return settingsStore;
+}
 
-export const evaluation_GoodBadTrialThreshold = writable(parseFloat(localStorage.evaluation_GoodBadTrialThreshold) || 4.0);
+function createBoolSettingsStore(key: string, defaultValue: boolean) {
+    return createSettingsStore(key, defaultValue, (input: string) => input === 'true');
+}
+function createNumberSettingsStore(key: string, defaultValue: number) {
+    return createSettingsStore(key, defaultValue, Number.parseFloat);
+}
 
-export const feedback_YellowThreshold = writable(parseFloat(localStorage.feedback_YellowThreshold) || 3.0);
+export const debugMode = createBoolSettingsStore("debugMode", DEFAULT_SETTINGS.debugMode);
+export const pauseInPracticePage = createBoolSettingsStore("pauseInPracticePage", DEFAULT_SETTINGS.pauseInPracticePage);
+export const debugPauseDurationSecs = createNumberSettingsStore("debugPauseDurationSecs", DEFAULT_SETTINGS.debugPauseDurationSecs);
+export const evaluation_GoodBadTrialThreshold = createNumberSettingsStore("evaluation_GoodBadTrialThreshold", DEFAULT_SETTINGS.evaluation_GoodBadTrialThreshold);
+export const feedback_YellowThreshold = createNumberSettingsStore("feedback_YellowThreshold", DEFAULT_SETTINGS.feedback_YellowThreshold);
+export const feedback_GreenThreshold = createNumberSettingsStore("feedback_GreenThreshold", DEFAULT_SETTINGS.feedback_GreenThreshold);
 
-export const feedback_GreenThreshold = writable(parseFloat(localStorage.feedback_GreenThreshold) || 4.2);
-
-debugMode.subscribe((value) => localStorage.debugMode = String(value));
-
-pauseInPracticePage.subscribe((value) => localStorage.pauseInPracticePage = String(value));
-
-debugPauseDurationSecs.subscribe((value) => localStorage.debugPauseDurationSecs = String(value));
-
-evaluation_GoodBadTrialThreshold.subscribe((value) => localStorage.evaluation_GoodBadTrialThreshold = String(value));
-
-feedback_YellowThreshold.subscribe((value) => localStorage.feedback_YellowThreshold = String(value));
-
-feedback_GreenThreshold.subscribe((value) => localStorage.feedback_GreenThreshold = String(value));
+// Reset all variables to their default values
+export function resetSettingsToDefault() {
+    debugMode.set(DEFAULT_SETTINGS.debugMode);
+    pauseInPracticePage.set(DEFAULT_SETTINGS.pauseInPracticePage);
+    debugPauseDurationSecs.set(DEFAULT_SETTINGS.debugPauseDurationSecs);
+    evaluation_GoodBadTrialThreshold.set(DEFAULT_SETTINGS.evaluation_GoodBadTrialThreshold);
+    feedback_YellowThreshold.set(DEFAULT_SETTINGS.feedback_YellowThreshold);
+    feedback_GreenThreshold.set(DEFAULT_SETTINGS.feedback_GreenThreshold);
+}
