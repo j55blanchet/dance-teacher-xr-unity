9306e59408b717c170ccfb29d9bf6b580a699750
Julien Blanchet <j55blanchet@gmail.com>
2023-10-20 15:42:45 -0400

    Fixing back button on dance tree page

--- svelte-web-frontend/src/routes/teachlesson/[danceTreeId]/+page.svelte
+++ svelte-web-frontend/src/routes/teachlesson/[danceTreeId]/+page.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
 import DanceTreePage from "$lib/pages/DanceTreePage.svelte";
 import type { DanceTree, Dance } from '$lib/data/dances-store';
+import { navbarProps } from "$lib/elements/NavBar.svelte";
 
 /** @type {import('./$types').PageData} */    
 export let data;
 const dance: Dance = data.dance;
 const danceTree: DanceTree = data.danceTree;
+
+$: {
+	navbarProps.set({
+		collapsed: false,
+		pageTitle: `${dance.title}`,
+		back: {
+			url: '/',
+			title: 'Menu',
+		},
+	});
+}
 
 </script>
 
