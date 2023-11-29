9ca9d9de7932949d85382125018b774472d8e031
Julien Blanchet <j55blanchet@gmail.com>
2023-11-29 14:42:14 -0500

    Adding redirect on client-side load code

--- svelte-web-frontend/src/routes/+page.ts
+++ svelte-web-frontend/src/routes/+page.ts
@@ -1,12 +1,17 @@
 // since there's no dynamic data here, we can prerender
 
+import { redirect } from "@sveltejs/kit";
 import type { PageLoad } from "./$types";
 
 // it so that it gets served as a static asset in production
 export const prerender = true;
 
-export const load: PageLoad = async ({ url, data}) => {
+export const load: PageLoad = async ({ url, data }) => {
     
+    if (data.session) {
+        throw redirect(302, '/menu')
+    }
+
     return {
         server_url: data.server_url,
         origin: url.origin,
