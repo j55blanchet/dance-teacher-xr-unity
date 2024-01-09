1a22d70aa63d7ae9be85a17ec77a554a9c206be7
Julien Blanchet <j55blanchet@gmail.com>
2024-01-09 01:40:49 -0500

    improving migration (for future use)

--- svelte-web-frontend/supabase/migrations/20240109060039_create-acrivitystep-progress-table.sql
+++ svelte-web-frontend/supabase/migrations/20240109060039_create-acrivitystep-progress-table.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS LearningStepProgress (
     id SERIAL PRIMARY KEY,
     user_id UUID REFERENCES profiles(id),
     timestamp TIMESTAMP NOT NULL DEFAULT NOW(),
+    dance_id UUID NOT NULL,
     tree_name TEXT NOT NULL,
     activity_name TEXT NOT NULL,
     step_name TEXT NOT NULL,
