6b9ce879c82a53560b1680334b0b6c202eed7a02
Julien Blanchet <j55blanchet@gmail.com>
2024-01-09 01:04:32 -0500

    created supabase migraiton script - but will abandon this for now

---
+++ svelte-web-frontend/supabase/migrations/20240109060039_create-acrivitystep-progress-table.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE IF NOT EXISTS LearningStepProgress (
+    id SERIAL PRIMARY KEY,
+    user_id UUID REFERENCES profiles(id),
+    timestamp TIMESTAMP NOT NULL DEFAULT NOW(),
+    tree_name TEXT NOT NULL,
+    activity_name TEXT NOT NULL,
+    step_name TEXT NOT NULL,
+    state jsonb NOT NULL
+);
